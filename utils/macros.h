545608004fc59bc51fce29096e0c2b06bd6103af
Ra6nar0k21 | Michael <38357618+michaeltrosin@users.noreply.github.com>
2021-03-21 13:45:57 +0100

    Test

--- utils/macros.h
+++ utils/macros.h
@@ -35,6 +35,8 @@
 #error "NOT SUPPORTED"
 #endif
 
+#define test_macro
+
 #define BIT(x) (1 << (x))
 
 #define BITFIELD(enum)                                                                                                                               \
